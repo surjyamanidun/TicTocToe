0742ac59974920bd2aa7b27f463b3b4b0c00f866
surjya <surjyamaniduan@gmail.com>
2021-10-12 23:33:11 +0530

    Add:In between player and Computer

--- src/main/java/tictactoe/TicTacToeGame.java
+++ src/main/java/tictactoe/TicTacToeGame.java
@@ -1,22 +1,51 @@
 package tictactoe;
-
+import java.util.*;
 public class TicTacToeGame {
 
-		 static  char[] board = new char[10];
+	    static  char[] board = new char[10];
+	    private static char cross ='X';
+	    private static char zero ='O';
+	    private static char player;
+	    private static char computer;
+	    public static char[]  creatingBoard() {
 
-		    public static char[]  creatingBoard() {
 
+	        for (int i = 1; i < board.length; i++) {
+	            board[i] = ' ';
+	        }
+	        return board;
+	    }
+	    private static void allowPlayerToChoose()
+	    {
+	        System.out.println("Enter X or O you want to choose");
+	        Scanner sc = new Scanner(System.in);
+	        String input = sc.nextLine();
+	        player = input.charAt(0);
 
-		        for (int i = 1; i < board.length; i++) {
-		            board[i] = ' ';
-		        }
-		        return board;
+	        if(player == cross)
+	        {
+	            player = cross;
+	            computer = zero;
+	            System.out.println("player symbol : " + player);
 
-		    }
+	        }
+	        else if(player == zero)
+	        {
+	            player = zero;
+	            computer = cross;
+	            System.out.println("player symbol : " + player);
 
-		    public static void main(String args[]) {
+	        }
+	        else
+	        {
+	            System.out.println("Invalid Character");
+	        }
+	    }
 
-		        System.out.println("Welcome to TicTacToe Board Game");
-		        board = creatingBoard();
-		    }
+	    public static void main(String args[]) {
+
+	        System.out.println("Welcome to TicTacToe Board Game");
+	        board = creatingBoard();
+	        allowPlayerToChoose();
+	    }
 	}
